6a230648d0ad2aef45002011c8fed1c57667a552
Elvis Mao <info@elvismao.com>
2025-03-24 15:46:50 +0800

    feat: console easter egg

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,10 @@
-console.log(
-    "%c既然你看到這裡了，那趕快去報名 SITCON Camp！",
-    "font-size: 40px; background-color: yellow; color: red;"
-);
+const ad = () =>
+    console.log(
+        "%c既然你看到這裡了，那趕快去報名 SITCON Camp！",
+        "font-size: 40px; background-color: #f56f21; color: #000;font-family:system-ui;"
+    );
+
+ad();
 
 // Scroll
 window.addEventListener("scroll", function () {
@@ -12,7 +15,7 @@ window.addEventListener("scroll", function () {
 // Animate on Scroll
 function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
-    return rect.bottom < 0 || (rect.top) > window.innerHeight;
+    return rect.bottom < 0 || rect.top > window.innerHeight;
 }
 
 function addClassToVisibleElements() {
@@ -25,3 +28,16 @@ function addClassToVisibleElements() {
 
 document.addEventListener("scroll", addClassToVisibleElements);
 addClassToVisibleElements();
+
+document.addEventListener("keydown", e => {
+    // Check for common DevTools shortcuts
+    if (
+        // Ctrl+Shift+I (Windows/Linux) or Cmd+Option+I (Mac)
+        (e.ctrlKey && e.shiftKey && e.key === "I") ||
+        // Ctrl+Shift+J (Windows/Linux) or Cmd+Option+J (Mac)
+        (e.ctrlKey && e.shiftKey && e.key === "J") ||
+        // F12
+        e.key === "F12"
+    )
+        ad();
+});
