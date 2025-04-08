c29ffeca8815589b40d231e6bbee874ec6180f5f
KoukeNeko <KoukeNeko@users.noreply.github.com>
2025-04-08 02:25:52 +0000

    deploy: 78984e60915186598b8d7f670c9893d57ba7f9ae

--- components/index.js
+++ components/index.js
@@ -1,4 +1,5 @@
 // Import all components
 import './navigation-bar.js';
 import './site-footer.js';
-import './scroll-to-top.js'; 
+import './scroll-to-top.js';
+import '../lib/pjax-handler.js'; // Add PJAX support for smoother page transitions 
